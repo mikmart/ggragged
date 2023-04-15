857f0329ce92b5caa93742b60a216fde5a87d95a
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2023-04-15 22:55:40 +0100

    Refactor tests

--- tests/testthat/test-facet_ragged_cols.R
+++ tests/testthat/test-facet_ragged_cols.R
@@ -1,9 +1,11 @@
 tbl <- data.frame(foo = c("A", "B", "B"), bar = 1:3, x = 1:3, y = 3:1)
-p <- ggplot(tbl, aes(x, y)) + facet_ragged_cols(vars(bar), vars(foo))
 
 test_that("axes and strips don't overlap", {
-  p <- p + scale_x_continuous(sec.axis = dup_axis())
+  p <- ggplot(tbl, aes(x, y)) + scale_x_continuous(sec.axis = dup_axis())
+  
+  p <- p + facet_ragged_cols(vars(bar), vars(foo))
   vdiffr::expect_doppelganger("x axes on both sides, default", p)
+
   p <- p + facet_ragged_cols(vars(bar), vars(foo), switch = "x")
   vdiffr::expect_doppelganger("x axes on both sides, switched", p)
 })

5deeda17ef703b5d98b85393792f637f26b925cf
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2024-10-05 20:54:10 +0100

    Add regression test for `coord_flip` issue

--- tests/testthat/test-facet_ragged_cols.R
+++ tests/testthat/test-facet_ragged_cols.R
@@ -9,3 +9,9 @@ test_that("axes and strips don't overlap", {
   p <- p + facet_ragged_cols(vars(bar), vars(foo), switch = "x")
   vdiffr::expect_doppelganger("x axes on both sides, switched", p)
 })
+
+test_that("correct axes are rendered with free and coord_flip (#2)", {
+  p <- ggplot(tbl, aes(x, y)) + coord_flip()
+  p <- p + facet_ragged_cols(vars(bar), vars(foo), scales = "free_x")
+  vdiffr::expect_doppelganger("x axes have varying ranges when free", p)
+})
